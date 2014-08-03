361672068edde6fd4f9be16abbbd1b4e7ddc7071
Markus Brueckner <namenlos@geekbetrieb.de>
2014-08-03 22:54:48 +0200

    - added: documentation - fixed: some cleanup

--- logging_utils.hxx
+++ logging_utils.hxx
@@ -3,6 +3,12 @@
 
 #include "logging.hxx"
 
+/**
+ * Return a string representation of the given trace level
+ * 
+ * \param tl The trace level to transform.
+ * \return The string representation of the given trace level.
+ */
 inline char const * const levelName(TraceLevel tl)
 {
     if (tl == LEVEL_TRACE) {
@@ -13,6 +19,12 @@ inline char const * const levelName(TraceLevel tl)
     }
 }
 
+/**
+ * Return a string representation of the given log level
+ * 
+ * \param ll The log level to transform.
+ * \return The string representation of the given log level.
+ */
 inline char const * const levelName(LogLevel ll)
 {
     switch (ll) {
@@ -31,6 +43,25 @@ inline char const * const levelName(LogLevel ll)
         default:
             return "UNKNOWN";
             break;
+    }
+}
+
+    
+/**
+* Get the full name of a logger. The canonical name consists of the
+* full chain up to the root logger separated by the given separator.
+* 
+* \param l The logger to return the canonical name on.
+* \param separator The separator to join the canonical name parts with.
+* \return The full canonical name of the logger
+*/
+template <typename LoggerType> std::string canonicalName(LoggerType const &l, std::string const &separator = "::")
+{
+    if (l.parent() && l.parent()->name().size() > 0) {
+        return canonicalName(*(l.parent()), separator)+separator+l.name();
+    }
+    else {
+        return l.name();
     }
 }
 
